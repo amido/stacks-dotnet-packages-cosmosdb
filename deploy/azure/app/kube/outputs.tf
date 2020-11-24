cc920d5e589f1a64a2a5f0e2caaad71b268ceef1
Jack Blower <Jack@elvenspellmaker.co.uk>
2020-11-24 16:49:38 +0000

    feat: Adds YAML Linting and TF Validation

---
+++ deploy/azure/app/kube/outputs.tf
@@ -0,0 +1,15 @@
+output "cosmosdb_account_name" {
+  description = "CosmosDB account name"
+  value       = module.cosmosdb.cosmosdb_account_name
+}
+
+output "cosmosdb_endpoint" {
+  description = "Endpoint for accessing the DB CRUD"
+  value       = module.cosmosdb.cosmosdb_endpoint
+}
+
+output "cosmosdb_primary_master_key" {
+  description = "Primary Key for accessing the DB CRUD, should only be used in applications running outside of AzureCloud"
+  sensitive   = true
+  value       = module.cosmosdb.cosmosdb_primary_master_key
+}
