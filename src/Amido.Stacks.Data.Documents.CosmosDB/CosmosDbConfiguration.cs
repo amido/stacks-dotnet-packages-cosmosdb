29a2305feb9e9bc5ac684405e32843b224b389a7
Yuki <42913210+YukiK710@users.noreply.github.com>
2021-06-23 09:42:23 +0100

    Removed unused properties

--- src/Amido.Stacks.Data.Documents.CosmosDB/CosmosDbConfiguration.cs
+++ src/Amido.Stacks.Data.Documents.CosmosDB/CosmosDbConfiguration.cs
@@ -6,19 +6,10 @@ namespace Amido.Stacks.Data.Documents.CosmosDB
     public class CosmosDbConfiguration
     {
         public string DatabaseAccountUri { get; set; }
-
         public string DatabaseName { get; set; }
         public string DatabasePrimaryKey { get; set; }
-
-        //public string SecurityKey { get; set; }
         public Secret SecurityKeySecret { get; set; }
         
-
-
-        //public bool ReturnETags { get; set; }
-
-        //public bool ReturnRequestCharge { get; set; }
-
         /// <summary>
         /// Max query concurrency:
         ///  -1 = Dynamic, defined by server
@@ -43,8 +34,5 @@ namespace Amido.Stacks.Data.Documents.CosmosDB
         /// </summary>
         public TimeSpan RequestTimeout { get; set; } = TimeSpan.FromSeconds(30);
 
-        //public int? ResponseContinuationTokenLimitInKb
-        //public bool? EnableScanInQuery
-        //public bool? EnableLowPrecisionOrderBy
     }
 }
